c8251a61473c0646cd8e1098af26290192ed2ae8
JaViKo500 <javikogutierrez64@gmail.com>
2024-06-23 12:51:19 -0500

    service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
+   constructor(
+      private readonly carService: CarsService
+   ) {
+      
+   }
    @Get()
-   getCar(){
-      return ['toyota', 'honda'];
+   getCars(){
+      return this.carService.findAll();
+   }
+
+   @Get( ':id' )
+   getCarById( @Param('id') id: string = '0' ){
+      return this.carService.findById( Number(id) );
    }
 }
