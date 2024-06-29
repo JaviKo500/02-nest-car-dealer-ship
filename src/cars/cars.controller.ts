430987ef7068be88cbabf69889025379d8e8c1b4
JaViKo500 <javikogutierrez64@gmail.com>
2024-06-29 16:39:06 -0500

    validate uuid

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,7 +14,7 @@ export class CarsController {
    }
 
    @Get( ':id' )
-   getCarById( @Param('id') id: string = '0' ){
+   getCarById( @Param('id', new ParseUUIDPipe( { version: '4' } ) ) id: string = '0' ){
       return this.carService.findById( id );
    }
 
